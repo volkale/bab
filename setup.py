9ba221c5a3da8f9a0abdba28cc0db01a5f96a1b0
Alexander Volkmann <alexv@gmx.de>
2019-04-14 20:33:53 +0200

    addd setup and requirements files

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='bab',
+    version='0.0.1',
+    description='Bayesian estimation for A / B testing',
+    author='Alexander Volkmann',
+    packages=['bab'],
+    package_dir={'bab': 'bab'},
+    package_data={'bab': ['tests/data/*.csv']},
+    url='https://github.com/volkale/bab',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+
+)
