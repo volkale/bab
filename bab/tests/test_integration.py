82395aa961ac9f20dc09a9a2510dfb4a70c820c4
Alexander Volkmann <alexv@gmx.de>
2019-04-19 14:42:57 +0200

    fix integration test

--- bab/tests/test_integration.py
+++ bab/tests/test_integration.py
@@ -1,8 +1,7 @@
-from bab.mcmc import get_mcmc, get_stan_model, plot_posteriors
+from bab.mcmc import get_mcmc, get_stan_model
 from bab.make_data import make_data
 
 
-y1, y2 = data = make_data(0, 1, 1, 2, 10, percent_outliers=0, sd_outlier_mfactor=2.0, rand_seed=1)
+y1, y2 = data = make_data(0, 1, 1, 2, 10, percent_outliers=0, sd_outlier_factor=2.0, rand_seed=1)
 stan_model = get_stan_model()
 mcmc = get_mcmc(stan_model, y1, y2)
-plot_posteriors(mcmc, 'mu')
