de41c6e24f03d34a2cc1cf4b93f488aa85d5800c
Alexander Volkmann <alexv@gmx.de>
2019-06-27 23:24:01 +0300

    add get_posterior function to plots module

--- bab/plots.py
+++ bab/plots.py
@@ -22,6 +22,31 @@ def get_pairs(mcmc):
     plt.show()
 
 
+def get_posterior(mcmc):
+
+    data = az.from_pystan(
+        posterior=mcmc,
+        posterior_predictive=['y1_pred', 'y2_pred'],
+        observed_data=['y1', 'y2'],
+        log_likelihood='log_lik',
+        coords={
+            'group_mu': ['Group 1', 'Group 2'],
+            'group_sigma': ['Group 1', 'Group 2']
+        },
+        dims={
+            'mu': ['group_mu'], 'sigma': ['group_sigma']
+        }
+    )
+
+    _ = az.plot_posterior(  # NOQA
+        data,
+        var_names=['mu', 'sigma', 'log_nu'],
+        credible_interval=0.95,
+        figsize=(10, 10)
+    )
+    plt.show()
+
+
 def get_forest(mcmc):
 
     data = az.from_pystan(
