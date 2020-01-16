e1cf4d9b186cea035bcac9e0e643c417a6b2630d
Alexander Volkmann <alexv@gmx.de>
2020-01-17 00:37:25 +0100

    change dtype of variables from int tp float

--- bab/tests/test_make_data.py
+++ bab/tests/test_make_data.py
@@ -3,8 +3,8 @@ from bab.make_data import make_data
 
 
 def test_make_data():
-    mu1 = mu2 = 0
-    sigma1 = sigma2 = 1
+    mu1 = mu2 = 0.
+    sigma1 = sigma2 = 1.
     n_per_group = int(1e6)
 
     y1, y2 = make_data(mu1, sigma1, mu2, sigma2, n_per_group, percent_outliers=0, sd_outlier_factor=2.0, rand_seed=0)
