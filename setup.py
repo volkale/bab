2a8bcf6c558165c09547155282756ed7825f8139
Alexander Volkmann <alexv@gmx.de>
2020-01-18 23:23:57 +0100

    fix requirements

--- setup.py
+++ setup.py
@@ -6,20 +6,31 @@ except ImportError:
     from distutils.core import setup
 
 
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().splitlines()
+
+
+with open('requirements_test.txt', 'r') as f:
+    tests_require = f.read().splitlines()
+
+
 setup(
     name='bab',
-    version='0.1.0',
+    version='0.1.2',
     description='Bayesian estimation for A / B testing',
     author='Alexander Volkmann',
     author_email='alexv@gmx.de',
     packages=['bab'],
     package_dir={'bab': 'bab'},
-    package_data={'bab': ['tests/data/*.csv']},
+    package_data={'bab': ['model.stan', 'tests/data/*.csv']},
+    include_package_data=True,
+    setup_requires=['pytest-runner>=4.2', 'flake8'],
+    install_requires=install_requires,
+    tests_require=tests_require,
     url='https://github.com/volkale/bab',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.7'
     ]
-
 )
